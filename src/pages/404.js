947200737292771a03ba278673edc4ec85259100
aprietof <adrianprieto00@gmail.com>
2020-08-24 10:43:56 -0400

    Initial commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,17 @@
-import React from "react";
-
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
+import React from 'react';
+import Layout from '../components/layout';
+import abductionIllustration from '../images/abduction-illustration.svg';
 
 function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
       <div>
         <img
           alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
+          className="block w-1/2 mx-auto"
           src={abductionIllustration}
         />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="inline-block p-3 my-8 text-2xl font-bold bg-yellow-400">
           Looks like this page is a ghost that got abducted by aliens...
         </h2>
       </div>
