0cfd27cdafd339bd3ece0c4bc00af7e5c6907510
aprietof <adrianprieto00@gmail.com>
2020-08-24 12:42:37 -0400

    Add grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import Layout from '../components/layout';
 
 function IndexPage() {
-  return <Layout>test</Layout>;
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-1/2 border border-red-500 md:w-1/5 square">
+        <div className="absolute flex flex-wrap w-full h-full">
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+        </div>
+      </div>
+      <div className="w-1/2 border border-red-500 md:w-1/5 square">
+        <div className="absolute flex flex-wrap w-full h-full">
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+        </div>
+      </div>
+      <div className="w-full border border-red-500 md:w-1/5 square">
+        <div className="absolute flex flex-wrap w-full h-full bg-red-400"></div>
+      </div>
+      <div className="w-1/2 border border-red-500 md:w-1/5 square">
+        <div className="absolute flex flex-wrap w-full h-full">
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+        </div>
+      </div>
+      <div className="w-1/2 border border-red-500 md:w-1/5 square">
+        <div className="absolute flex flex-wrap w-full h-full">
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+          <div className="w-1/2 bg-blue-800 border border-white square"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default IndexPage;
